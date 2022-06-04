e88401f3f245b69232af5e18cb0c1f6433f9b55c
Alex <alexiv.04@mail.ru>
2022-06-04 21:44:38 +0700

    Завершение переходов и дизайна

--- app/src/main/java/com/example/ivanov_pr12/MainActivity7.kt
+++ app/src/main/java/com/example/ivanov_pr12/MainActivity7.kt
@@ -5,17 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity7 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main7)
     }
     fun onClickStepsActivity(view: View){
-        val intent = Intent(this, MainActivity3::class.java)
+        val intent = Intent(this, MainActivity5::class.java)
         startActivity(intent)
     }
     fun onClickStepsActivity1(view: View){
-        val intent = Intent(this, MainActivity4::class.java)
+        val intent = Intent(this, MainActivity2::class.java)
         startActivity(intent)
     }
 }
